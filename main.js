b363850861fc40bd2fcd50ade4b0ffa2a3b18ca2
kevin <khesse92@gmail.com>
2018-03-20 17:43:15 -0400

    adding smooth scrolling properties

--- main.js
+++ main.js
@@ -2,13 +2,34 @@
 
 document.addEventListener("scroll", event => {
   if(( window.scrollY < 25) && (window.scrollY >= 20 )) {
-    window.scrollTo(0,900);
+    //window.scrollTo(0,900);
+    window.scroll({
+      top: 900,
+      left: 0,
+      behavior: 'smooth'
+    });
+
 } if(( window.scrollY > 950) && (window.scrollY < 1000 )) {
-    window.scrollTo(0,1700);
+  window.scroll({
+    top: 1800,
+    left: 0,
+    behavior: 'smooth'
+  });
+
 } if (( window.scrollY > 1800) && (window.scrollY < 1860)) {
-    window.scrollTo (0,2600);
+    //window.scrollTo (0,2600);
+    window.scroll({
+      top: 2600,
+      left: 0,
+      behavior: 'smooth'
+  });
 } if (( window.scrollY > 2700) && (window.scrollY < 2750)) {
-    window.scrollTo(0,3500);
+    //window.scrollTo(0,3500);
+    window.scroll({
+      top: 3500,
+      left: 0,
+      behavior: 'smooth'
+    });
 }
 })
 
