b2b287349d3a5ceaab4dbf74f47e4234ee50b530
kevin <khesse92@gmail.com>
2018-03-20 17:08:46 -0400

    added all scrollTo positions

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-//let scrollOne = window.scrollTo(0,500);
-//let scrollTwo = wondow.scrollTo(0,1000);
-//let scrollThree = window.scrollTo(1500);
-//let scrillFour = window.scrollTo(2000);
 
 
 document.addEventListener("scroll", event => {
-  if(window.scrollY < 20) {
-  window.scrollTo(0,1000);
+  if(( window.scrollY < 25) && (window.scrollY >= 20 )) {
+    window.scrollTo(0,900);
+} if(( window.scrollY > 950) && (window.scrollY < 1000 )) {
+    window.scrollTo(0,1700);
+} if (( window.scrollY > 1800) && (window.scrollY < 1860)) {
+    window.scrollTo (0,2600);
+} if (( window.scrollY > 2700) && (window.scrollY < 2750)) {
+    window.scrollTo(0,3500);
 }
 })
 
