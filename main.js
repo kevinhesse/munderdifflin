28d1aecea7d5f5f2cf00f0d3efa92df4b33d87f5
kevin <khesse92@gmail.com>
2018-03-20 16:15:55 -0400

    add scrollTo in addEventListener

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-let scrollOne = window.scrollTo(0,500);
-let scrollTwo = wondow.scrollTo(0,1000);
-let scrollThree = window.scrollTo(1500);
-let scrillFour = window.scrollTo(2000);
+//let scrollOne = window.scrollTo(0,500);
+//let scrollTwo = wondow.scrollTo(0,1000);
+//let scrollThree = window.scrollTo(1500);
+//let scrillFour = window.scrollTo(2000);
+
+
+document.addEventListener("scroll", event => {
+  if(window.scrollY < 20) {
+  window.scrollTo(0,1000);
+}
+})
+
+console.log("I work!");
