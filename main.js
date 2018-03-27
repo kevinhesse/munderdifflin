64ca185beaf57aa4011ef8d29d52175ae9f9e7c1
kevin <khesse92@gmail.com>
2018-03-27 16:19:36 -0400

    test commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-
+//scrolls to specific section upon scrolling
 document.addEventListener("scroll", event => {
   if(( window.scrollY < 25) && (window.scrollY >= 20 )) {
-    //window.scrollTo(0,900);
+console.log("")
     window.scroll({
       top: 900,
       left: 0,
@@ -17,14 +17,12 @@ document.addEventListener("scroll", event => {
   });
 
 } if (( window.scrollY > 1800) && (window.scrollY < 1860)) {
-    //window.scrollTo (0,2600);
     window.scroll({
       top: 2600,
       left: 0,
       behavior: 'smooth'
   });
 } if (( window.scrollY > 2700) && (window.scrollY < 2750)) {
-    //window.scrollTo(0,3500);
     window.scroll({
       top: 3500,
       left: 0,
